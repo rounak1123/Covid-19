67959a63769f023af0461a0be6b15a45d5227a88
Rounak <rounkagrwl@gmail.com>
2021-07-05 02:42:28 +0530

    Added a typeWriter effect

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,10 +1,24 @@
 import React from "react";
+import Typewriter from "typewriter-effect";
 import "./hero.css";
 
 export default function hero() {
   return (
     <div className="herosec">
-      <div className="conthero"> Welcome to our Covid Tracker</div>
+      <div className="conthero">
+        {" "}
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter
+              .typeString("Welcome to our Covid tracking application.")
+              .pauseFor(3000)
+              .deleteAll()
+
+              .typeString("Hope You Like It :) !")
+              .start();
+          }}
+        />
+      </div>
     </div>
   );
 }
